deb91f811b38a2ea91a4e2216a01a171fb7080a0
badgeminer <64038298+badgeminer@users.noreply.github.com>
2023-04-11 10:36:41 -0600

    gc

--- main.py
+++ main.py
@@ -6,10 +6,15 @@ pygame.init()
 scr = pygame.display.set_mode((500,500))
 nd = nodeium.node((255,0,0),(10,10,100,50),"start")
 n = nodeium.node((0,255,0),(120,10,50,50))
+ne = nodeium.node((0,255,0),(180,10,50,50),"end")
 n.incon[0].connd = nd.outcon[0]
 n.incon[1].connd = nd.outcon[1]
 nd.incon = []
 nds = [n,nd]
+
+
+
+
 while True:
     scr.fill((0,0,0))
     for i in nds:
